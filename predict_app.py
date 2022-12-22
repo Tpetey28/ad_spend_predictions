5b19872d7e59adc31b575e6924b21d0b8b0e05cc
Tpetey28 <106124486+Tpetey28@users.noreply.github.com>
2022-12-22 16:23:38 -0700

    Update predict_app.py

--- predict_app.py
+++ predict_app.py
@@ -5,6 +5,37 @@ import pickle
 import streamlit as st
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+
 #establishing key metrics
 lead_orders_22 = 2259
 total_leads_22 = 56468
@@ -293,5 +324,5 @@ def show_predict_page():
             plt.legend(fontsize=25, loc='upper left')
                 
             st.pyplot(fig)
-
+check_password()
 show_predict_page()
