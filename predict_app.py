4d66510065a1eb3c3bb22a149ee2546b57c25011
Tpetey28 <106124486+Tpetey28@users.noreply.github.com>
2022-12-23 14:11:18 -0700

    Update predict_app.py

--- predict_app.py
+++ predict_app.py
@@ -341,8 +341,7 @@ def check_password():
 if "password_correct" not in st.session_state:
         # First run, show input for password.
         st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
+            "Password", type="password", on_change=password_entered, key="password")
         return False
 elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
